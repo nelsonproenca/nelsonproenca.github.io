119a263ffdd1e6825f18eba000552ba2490eca1b
NELSON HENRIQUE DE ALMEIDA PROENCA <nelsonhaproenca@gmail.com>
2019-11-07 22:16:50 +0000

    Add files via upload
    Builld react project

---
+++ precache-manifest.7a69c2bce080b2341d0dbbb71484387b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d1fbb32f644644eca666996ce06037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d39a168ab3e7a364b16",
+    "url": "/static/css/main.1bd48116.chunk.css"
+  },
+  {
+    "revision": "c20c06c9ecb82f714356",
+    "url": "/static/js/2.291c32a8.chunk.js"
+  },
+  {
+    "revision": "3d39a168ab3e7a364b16",
+    "url": "/static/js/main.0b119696.chunk.js"
+  },
+  {
+    "revision": "57a56838963cf107df36",
+    "url": "/static/js/runtime-main.d5934866.js"
+  }
+]);
